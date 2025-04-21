432fb2064dd8d28da6a5250d3274127b56e3b06a
Thomas <thgi236@student.oaisd.org>
2025-04-21 14:43:12 -0400

    animations ish

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 
 public class Player : MonoBehaviour
 {
@@ -14,17 +16,18 @@ public class Player : MonoBehaviour
     public Transform lFoot;
     public Transform rFoot;
 
+    Animator animator;
+    SpriteRenderer sprite;
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        ContactFilter2D filter = new ContactFilter2D();
-        filter.SetLayerMask(groundLayer);
-        filter.useLayerMask = true;
-
         RaycastHit2D rHit = Physics2D.Raycast(rFoot.position, Vector2.down, 0.1f, groundLayer);
         RaycastHit2D lHit = Physics2D.Raycast(lFoot.position, Vector2.down, 0.1f, groundLayer);
         isFalling = (lHit || rHit) ? false : true;
@@ -33,11 +36,20 @@ public class Player : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
             {
+                animator.SetTrigger("Jump");
                 rigid.velocity = new Vector2(rigid.velocity.x, 0f);
                 rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
         }
         
         rigid.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rigid.velocity.y);
+
+        animator.SetBool("Run", Input.GetAxis("Horizontal") != 0);
+        if (Input.GetAxis("Horizontal") != 0) sprite.flipX = Input.GetAxis("Horizontal") < 0;
+
+        if (Input.GetMouseButtonDown(0)) animator.SetTrigger("Attack");
+        if (Input.GetMouseButtonDown(1)) animator.SetTrigger("Ability");
+
+        if (Input.GetKeyDown(KeyCode.E)) animator.SetTrigger("Hit");
     }
 }
