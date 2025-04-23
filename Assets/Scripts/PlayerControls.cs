5e2981499823b2058323c81d5faf386049275659
Thomas <thgi236@student.oaisd.org>
2025-04-23 14:38:42 -0400

    changed empty PlayerActions script to empty PlayerControls skript

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerControls : MonoBehaviour
+{
+    public UnityEvent test = new UnityEvent();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
