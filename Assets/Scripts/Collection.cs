a8c3f06250c52515be0824a1c574606d28c49021
Thomas <thgi236@student.oaisd.org>
2025-05-09 14:45:09 -0400

    Finished collections

--- Assets/Scripts/Collection.cs
+++ Assets/Scripts/Collection.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Collection : MonoBehaviour
 {
     public List<Collectible> collection = new List<Collectible>();
+    public GameObject ingredientPrefab;
+    GameObject ingredientPanel;
+    GameObject lastTriggered;
 
-    // Start is called before the first frame update
+    void Awake()
+    {
+        ingredientPanel = GameObject.Find("Ingredient Panel");
+
+        int i = 0;
+        foreach (Collectible collectible in collection)
+        {
+            GameObject instance = Instantiate(ingredientPrefab, ingredientPanel.transform);
+            instance.transform.localPosition = new Vector3(instance.transform.localPosition.x, -100 - (i * 150), 0);
+            instance.GetComponent<Image>().sprite = collectible.itemSprite;
+            instance.GetComponentInChildren<TextMeshProUGUI>().text = (collectible.amountNeeded > 1) ? $"{collectible.amountNeeded}" : "";
+            instance.name = collectible.itemName;
+            i++;
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -20,6 +39,7 @@ public class Collection : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        lastTriggered = other.gameObject;
         TryCollectItem(other.gameObject.name);
     }
 
@@ -39,6 +59,22 @@ public class Collection : MonoBehaviour
     void CollectItem(Collectible found)
     {
         found.amountNeeded--;
+        GameObject instance = GameObject.Find($"Ingredient Panel/{found.itemName}");
+        TextMeshProUGUI textComponent = instance.GetComponentInChildren<TextMeshProUGUI>();
+
+        if (found.amountNeeded > 0)
+        {
+            textComponent.text = (found.amountNeeded > 1) ? $"{found.amountNeeded}" : "";
+        }
+        else
+        {
+            instance.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            
+            textComponent.text = "";
+            textComponent.color = new Color(0, 255, 0, 1);
+        }
+
+        lastTriggered.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
     }
 }
 
